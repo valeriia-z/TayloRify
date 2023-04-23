ebaff65c3eca6f1c795f4cddde48e2115d599981
valeriia-z <valeriia.zinoveva@proton.me>
2023-04-23 17:41:31 +0200

    change in initial script

---
+++ scripts/01_retrieve_data.R
@@ -0,0 +1,42 @@
+# install required packages
+install.packages(c("spotifyr", "plyr", "tidyverse", "httr", "rvest", "stringr", 
+                   "ggthemes", "tidytext", "wordcloud", "ggridges", "wesanderson", 
+                   "yarrr", "knitr", "kableExtra", "radarchart"))
+
+# load required packages
+library(spotifyr)
+library(plyr)
+library(tidyverse)
+library(httr)
+library(rvest)
+library(stringr)
+library(ggthemes)
+library(tidytext)
+library(wordcloud)
+library(ggridges)
+library(wesanderson)
+library(yarrr)
+library(knitr)
+library(kableExtra)
+library(radarchart)
+
+# set your Spotify API credentials (need to provide your Client ID and Client Secret)
+client_id <- "40e05819d37d49b7a73804c728f1773c" # change to your own crenedtials
+client_secret <- "f97f4adeb138417c8a6d052257598b09"
+
+Sys.setenv(SPOTIFY_CLIENT_ID = client_id)
+Sys.setenv(SPOTIFY_CLIENT_SECRET = client_secret)
+
+# Generate an access token
+access_token <- get_spotify_access_token(client_id, client_secret)
+
+#using spoitfyr
+taylor <- get_artist_audio_features('taylor swift')
+
+taylor <- taylor %>% filter(album_name == "Taylor Swift" | album_name == "Fearless Platinum Edition" | 
+                              album_name == "Speak Now" | album_name == "Red" |
+                              album_name == "1989" | album_name == "reputation") 
+# The Spotify data for Taylor Swift changed a little in the week between my pulling it and posting this code.
+
+
+
